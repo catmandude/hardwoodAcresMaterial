f461cbf3ada2b933609a44f4f300342c9e8fa497
catmandude <ozziemiller555@gmail.com>
2018-04-25 21:35:43 -0500

    Adding material, fonts, header

---
+++ src/components/Home/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <headerToolbar />
+  </div>
+</template>
+
+<script>
+import headerToolbar from '../headerToolbar';
+
+export default {
+  name: 'home',
+  components: { headerToolbar },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/headerToolbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <md-toolbar md-elevation="0">
+      <h3 class="md-title">Transparent</h3>
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headerToolbar',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
