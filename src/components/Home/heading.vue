97adbb5e312b7c2b251a9bf57920bc39ecfe3bd7
catmandude <ozziemiller555@gmail.com>
2018-04-26 20:41:48 -0500

    Dope title

--- src/components/Home/heading.vue
+++ src/components/Home/heading.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <md-content><img src="../../../static/clouds-conifers.jpg"/></md-content>
-    <md-content class="md-primary">Primary</md-content>
-    <md-content class="md-accent">Accent</md-content>
+  <div class="md-layout md-alignment-top-center full">
+    <div class="md-layout-item md-size-33">
+      <md-content class="md-elevation-8">
+        <img src="../../../static/clouds-conifers.jpg"/>
+      </md-content>
+    </div>
+    <div class="md-layout-item md-size-50 text">
+      <h2 class="md-display-1">Some text about things and stuff for the website. Ya know?</h2>
+    </div>
+    <div class="md-layout-item md-size-33">
+      <md-content class="md-elevation-8">
+        <img class="right" src="../../../static/clouds-daylight.jpg"/>
+      </md-content>
+    </div>
   </div>
 </template>
 
@@ -14,11 +24,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+.full {
+  position: relative;
+}
+.text {
+  margin-top: 50px;
+  position: absolute;
+}
 .md-content {
-    width: 200px;
-    height: 200px;
-    display: inline-flex;
+    width: 400px;
+    height: auto;
     justify-content: center;
-    align-items: center;
+    margin: 50px;
+    text-align: center;
   }
+.right {
+  margin-top: -45px;
+}
+
+.titlemain {
+  position: absolute;
+}
 </style>
